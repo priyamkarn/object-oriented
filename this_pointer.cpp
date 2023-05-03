cbd1dad3f9433ba5a04e543fe36ad33793b8c6c7
priyam karn <83950448+priyamkarn@users.noreply.github.com>
2023-05-03 18:42:42 +0530

    Update this_pointer.cpp

--- this_pointer.cpp
+++ this_pointer.cpp
@@ -4,6 +4,15 @@
 //What is the type of "this" pointer ?
 //Ans : For a class X, the type of this pointer is ‘X* ‘.
 //      Also, if a member function of X is declared as const, then the type of this pointer is ‘const X *’
+/*
+    Constructors in C++
+    Constructor is used for construction of an object(i.e. initialization). Hence, It doesn't have return type.
+    We can do initialization using initializer list as well.
+    - We can have parameterized constructor (default parameters can also be used)
+    - We can have overloaded constructors as well
+    - NOTE: If you are declaring an array of objects (Complex c[3]; then you must have a default constructor because if we don't have default constructor, then
+      it will require the parameters to be passed and with statement Complex c[3], you can't do that)
+*/
 
 #include <iostream>
 using namespace std;
